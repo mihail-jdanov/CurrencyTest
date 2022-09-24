ea54bb3d0f3b75f7d5b1232fa27b25f28157e1f0
Mikhail Zhdanov <mihail.jdanov1993@gmail.com>
2022-09-24 13:53:17 +0700

    Created project

---
+++ CurrencyTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyTest
+//
+//  Created by Михаил Жданов on 24.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
